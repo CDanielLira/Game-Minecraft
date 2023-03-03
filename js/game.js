fd75c1934c9c79dc21cf76cf66e47394fae93239
LEPEG0 <116929271+LEPEG0@users.noreply.github.com>
2023-03-02 18:45:03 -0600

    Audio

--- js/game.js
+++ js/game.js
@@ -3,3 +3,11 @@
 function registro (){
     location.href = "registro.html";
 }
+
+window.onload = function(){
+    document.getElementById("Audios").play();
+};
+
+function PlayAudio(){
+    document.getElementById("Audios").play();
+}
